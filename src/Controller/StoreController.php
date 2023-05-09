455b3ba1a49c6a38a3066ed122ed4fbf3b6f01aa
markusVJH <markus.heinila@pp.inet.fi>
2023-05-09 12:16:09 +0300

    get request to fakeapi, 10 items displayed on front page

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StoreController extends AbstractController
 {
     #[Route('/store', name: 'app_store')]
-    public function index()
+    public function index(): Response
     {
-        return $this->render('store/index.html.twig');
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://fakestoreapi.com/products?limit=10');
+        if ($response->getStatusCode() == Response::HTTP_OK) {
+            $products = $response->toArray();
+            return $this->render('store/index.html.twig', [
+                'products' => $products
+            ]);
+        }
     }
 }
