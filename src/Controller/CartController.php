00d38bfb5e43ec3ce513431f383bff1af6e1312b
markusVJH <markus.heinila@pp.inet.fi>
2023-05-09 13:38:32 +0300

    buttons for items, and selected item displayed in cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index()
+    public function index(SessionInterface $session)
     {
-        return $this->render('cart/index.html.twig');
-    }    
+        $cart = $session->get('cart', []);
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart
+        ]);
+    }
 }
--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class StoreController extends AbstractController
 {
@@ -21,4 +23,26 @@ class StoreController extends AbstractController
             ]);
         }
     }
+    public function addToCart(Request $request, SessionInterface $session)
+    {
+        // Get the product ID from the POST request
+        $productId = $request->request->get('productId');
+        
+        // Fetch the product information from the API using the product ID
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://fakestoreapi.com/products/' . $productId);
+        $product = $response->toArray();
+    
+        // Store the product information in the session
+        $session->set('cart', [
+            'id' => $productId,
+            'title' => $product['title'],
+            'price' => $product['price'],
+            // Add any other relevant product information
+        ]);
+    
+        // Redirect to the cart page
+        return $this->redirectToRoute('cart');
+    }
+    
 }
